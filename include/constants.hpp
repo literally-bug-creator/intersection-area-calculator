a1a34724a61db2944d71cb03cc79c1c5e90ea944
literally-bug-creator <literallybugcreator@gmail.com>
2025-05-12 23:58:38 +0300

    Add new error constant - `INDEX_OUT_OF_RANGE`

--- include/constants.hpp
+++ include/constants.hpp
@@ -1,9 +1,14 @@
+#pragma once
+
 #include "error.hpp"
 
 namespace Messages {
     const std::string OK_ERROR = "";
+    const std::string OUT_OF_RANGE_ERROR = "Index out of range!";
 }
 
 namespace Errors {
-    const Error OK = make_error(ErrorCode::OK, Messages::OK_ERROR );
+    const Error OK = make_error( ErrorCode::OK, Messages::OK_ERROR );
+    const Error INDEX_OUT_OF_RANGE =
+        make_error( ErrorCode::RUNTIME_ERROR, Messages::OUT_OF_RANGE_ERROR );
 }
