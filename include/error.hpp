8734673aac7d5ca0da22a9e86894ef91f291e24e
literally-bug-creator <literallybugcreator@gmail.com>
2025-05-12 16:53:17 +0300

    Implement `Error` entity's interface

---
+++ include/error.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <string>
+
+enum ErrorCode {
+    OK,
+    INPUT_ERROR,
+    CALCULATION_ERROR
+};
+
+struct Error {
+    const ErrorCode code;
+    const std::string message;
+
+    Error( const ErrorCode code, const std::string& message ):
+        code( code ), message( message ) {}
+};
+
+const ErrorCode get_error_code( const Error& error );
+
+const std::string& get_error_message( const Error& error );
+
+const bool is_ok( const Error& error );
+
+const Error make_error( const ErrorCode code, const std::string& message );
